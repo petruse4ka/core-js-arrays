8a420006d61bf3bf19f7cc287e129be296182396
Konstantin Petrov <43613041+petruse4ka@users.noreply.github.com>
2024-11-22 21:58:08 +0100

    feat: add solution to task 16

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -279,8 +279,18 @@ function distinct(arr) {
  *    createNDimensionalArray(4, 2) => [[[[0, 0], [0, 0]], [[0, 0], [0, 0]]], [[[0, 0], [0, 0]], [[0, 0], [0, 0]]]]
  *    createNDimensionalArray(1, 1) => [0]
  */
-function createNDimensionalArray(/* n, size */) {
-  throw new Error('Not implemented');
+function createNDimensionalArray(n, size) {
+  let result;
+
+  if (n === 1) {
+    result = Array(size).fill(0);
+  } else {
+    result = Array(size)
+      .fill(0)
+      .map(() => createNDimensionalArray(n - 1, size));
+  }
+
+  return result;
 }
 
 /**
